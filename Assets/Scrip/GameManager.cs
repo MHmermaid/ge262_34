1a643c480398d16a24d55f773c6df862d17385ca
MHmermaid <kanyaphak.pras@bumail.net>
2023-04-07 20:20:10 +0700

    Finish Gen Candidate

---
+++ Assets/Scrip/GameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject staffPrefab;
+    public GameObject staffParent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateCandidate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void GenerateCandidate()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            GameObject staffObj = Instantiate(staffPrefab, staffParent.transform);
+            Staff s = staffObj.GetComponent<Staff>();
+            
+            s.InitCharID(i);
+            s.ChangeCharSkin();
+        }
+    }
+}
